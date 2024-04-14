b5dc8a705f33d09e5b65e611b33b2bc315824bc3
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-04-14 19:15:33 +0530

    src: added structs for cli command

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main 
 
 import "fmt"
+import "bufio"
+import "os"
+
+
+type CommandType struct {
+  name string 
+  descp string 
+  callback func() error 
+}
+
+const cmds := map[string]CommandType{
+  "help": {
+    name: "help",
+    descp: "displays a help message",
+    callback: cmdHelp,
+  },
+  "exit": {
+    name: "exit",
+    descp: "Exit the Pokedex",
+    callback: cmdExit,
+  }
+}
+
+
 
 func main(){
-  fmt.Println("Hello, world!")
+  scanner := bufio.NewScanner(os.Stdin)
+
+  for ;; {
+  
+  }
 }
